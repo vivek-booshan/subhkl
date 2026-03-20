0fe105a7dff8fb94fc2fe5733ba7b229f5a6dcb1
vivek-booshan <vivek.booshan@gmail.com>
2026-03-20 15:42:07 -0400

    add dataclasses and consts to init

---
+++ src/subhkl/core/__init__.py
@@ -0,0 +1,10 @@
+from .crystallography import Lattice, LATTICE_CONFIG, LATTICE_CONSTRAINTS
+from .experiment import ExperimentData, PeaksData
+
+__all__ = [
+    "Lattice",
+    "LATTICE_CONFIG",
+    "LATTICE_CONSTRAINTS",
+    "ExperimentData",
+    "PeaksData",
+]
